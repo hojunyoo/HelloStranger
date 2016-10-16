6e48b06b2dd28c1319c33b224aa7976391ede7a5
sejinq <thstpwlsq@gmail.com>
2016-10-16 15:07:56 +0900

    JoinActivity intent 추가 > taboo setting acivity 추가 해서 넘겨야 함, taboo setting xml 작업중

--- app/src/main/java/com/example/secpc/hellostranger/activity/JoinActivity.java
+++ app/src/main/java/com/example/secpc/hellostranger/activity/JoinActivity.java
@@ -2,6 +2,8 @@ package com.example.secpc.hellostranger.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.secpc.hellostranger.R;
 
@@ -11,5 +13,12 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        Button next = (Button)findViewById(R.id.LoginActivity_button_login);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/secpc/hellostranger/activity/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.secpc.hellostranger.activity;
+
+import android.app.Application;
+
+/**
+ * Created by secpc on 2016-10-16.
+ */
+public class MyApplication extends Application {
+
+}
