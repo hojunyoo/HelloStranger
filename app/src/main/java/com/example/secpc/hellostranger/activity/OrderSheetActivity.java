7369a88f5605dbffc550357175d1ecdd8b775bb4
dobeeisfree <jubee0124@gmail.com>
2016-10-25 20:13:36 +0900

    주문서 작성 버튼 2개 꽉차게 배치 #16
    그리고 사진 속정을 `fitXY`로 해줌.

--- app/src/main/java/com/example/secpc/hellostranger/activity/OrderSheetActivity.java
+++ app/src/main/java/com/example/secpc/hellostranger/activity/OrderSheetActivity.java
@@ -25,9 +25,9 @@ public class OrderSheetActivity extends AppCompatActivity {
 
         List<Dumy_Order> orders = new ArrayList<>();
         Dumy_Order[] order = new Dumy_Order[ORDER_SIZE];
-        order[0] = new Dumy_Order(R.drawable.dumy_tteokbokki, "떡볶이(Tteokbokki)", "2", "3,000");
-        order[1] = new Dumy_Order(R.drawable.dumy_sundae, "순대(Sundae)", "1", "4,000");
-        order[2] = new Dumy_Order(R.drawable.dumy_fried_somthin, "튀김(Twigim)", "3", "2,000");
+        order[0] = new Dumy_Order(R.drawable.dumy_tteokbokki, "떡볶이(Tteokbokki)", "2", "₩ 3,000  ");
+        order[1] = new Dumy_Order(R.drawable.dumy_sundae, "순대(Sundae)", "1", "₩ 4,000  ");
+        order[2] = new Dumy_Order(R.drawable.dumy_fried_somthin, "튀김(Twigim)", "3", "₩ 2,000  ");
 
         for(int i = 0; i < ORDER_SIZE; i++) {
             orders.add(order[i]);
