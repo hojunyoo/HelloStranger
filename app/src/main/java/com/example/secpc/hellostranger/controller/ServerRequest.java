ba90c4e562ff993ae1aa21bb7495a17ebd1cb6a0
se-jin son <thstpwlsq@gmail.com>
2016-12-04 16:01:22 +0900

    주석 설명 및 회원가입, 로그인 데이터 파싱 완료

--- app/src/main/java/com/example/secpc/hellostranger/controller/ServerRequest.java
+++ app/src/main/java/com/example/secpc/hellostranger/controller/ServerRequest.java
@@ -3,38 +3,35 @@ package com.example.secpc.hellostranger.controller;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.secpc.hellostranger.activity.MyApplication;
 import com.example.secpc.hellostranger.data.Store;
 import com.example.secpc.hellostranger.data.User;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Created by sejini on 2016-11-07.
- */
 
 public class ServerRequest extends Application{
+    //파싱 값을 저장 및 전달할 user instance
      public static User instanceuser = null;
     Store store;
+
 
     //기본 url 주소
     public static String SeverUrl ="https://hello-stranger-dobeeisfree.c9users.io/v1/";
     //넘겨줄 때 user/new? 그리고 그 뒤 값들 붙여줘야함.
 
+    //회원가입 시 서버에 날려줄 url 형식을 맞추는 함수 여러 user name, value들을 "&"로 붙여 보내주면된다.
     public static String joinURL(String name, String value){
         return "foreigners["+name+"]="+value;
     }
 
 
+    //회원가입, 로그인 시 json parsing함수
     public static void sendUserRequest(Context context, String url){
         RequestQueue queue = Volley.newRequestQueue(context);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
@@ -42,17 +39,8 @@ public class ServerRequest extends Application{
                     @Override
                     public void onResponse(JSONObject response) {
                            try {
-                                //정보를 담을 곳에 담아 보내준다.
-//                               user.setUserId(response.getString("id"));
-//                                String name = response.getString("name");
-//                                Log.e("name : ", user.getName());
-//                                user.setPassword(response.getString("password"));
-//                                Log.e("pass : ", user.getPassword());
-//                                String language = response.getString("lang");
-//                                user.setLanguage(Integer.parseInt(response.getString("lang")));
-//                                Log.e("lang : ", String.valueOf(user.getLanguage()));
-//                               user.setTaboo(Integer.parseInt(response.getString("for_taboo")));
-//                                Log.e("taboo : ", String.valueOf(response.getString("for_taboo")));
+                               //getString()으로 받아올 json 데이터의 name을 넣어주고 그에 맞는 값들을 바로 user 객체 생성자에 넣어 생성해 준다.
+                               // 생성 후에 ServerRequest의 instanceuser에 담아 함수를 호출한 후 값을 전달받는다.
 
                                User user = new User(response.getString("id"), response.getString("name"), response.getString("password"), Integer.parseInt(response.getString("lang"))
                                , null, Integer.parseInt(response.getString("for_taboo")));
